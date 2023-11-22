8abedca1c8cba08ac05779f00debfee57740d8f2
hkeonw <hkeonw@163.com>
2023-11-22 22:05:28 +0800

    图片列表

--- api/images_api/image_list.go
+++ api/images_api/image_list.go
@@ -1,1 +1,40 @@
 package images_api
+
+import (
+	"github.com/gin-gonic/gin"
+	"gvb_server/global"
+	"gvb_server/models"
+	"gvb_server/models/res"
+)
+
+type Page struct {
+	Page  int    `form:"page"`
+	Key   string `form:"key"`
+	Limit int    `form:"limit"`
+	Sort  string `form:"sort"`
+}
+
+func (ImagesApi) ImageListView(c *gin.Context) {
+	var imageList []models.BannerModel
+	var cr Page
+	err := c.ShouldBindQuery(&cr)
+	if err != nil {
+		res.FailWithCode(res.ArgumentError, c)
+		return
+	}
+	// 总数
+	count := global.DB.Find(&imageList).RowsAffected
+	if count > 0 {
+		offset := cr.Limit * (cr.Page - 1)
+		if offset <= 0 {
+			offset = 0
+		}
+		global.DB.Limit(cr.Limit).Offset(offset).Order(cr.Sort).Find(&imageList)
+	}
+
+	res.OkWithData(gin.H{
+		"total": count,
+		"list":  imageList,
+	}, c)
+	return
+}
--- routers/images_router.go
+++ routers/images_router.go
@@ -5,5 +5,5 @@ import "gvb_server/api"
 func (router RouterGroup) ImagesRouter() {
 	ImagesApi := api.ApiGroupApp.ImagesApi
 	router.POST("images", ImagesApi.ImageUploadView)
-	//router.PUT("settings/:name", ImagesApi.SettingsInfoUpdate)
+	router.GET("images", ImagesApi.ImageListView)
 }
